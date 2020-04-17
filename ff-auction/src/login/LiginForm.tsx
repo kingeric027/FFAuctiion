d0d243c63f5e7ebdf7f0f42fee26196a67c0b6e8
Eric King <eking@four51.com>
2020-04-16 22:05:34 -0500

    update to login form

--- ff-auction/src/login/LiginForm.tsx
+++ ff-auction/src/login/LiginForm.tsx
@@ -1,60 +1,79 @@
 import React, { useState } from 'react';
-import { Typography, Button, Dialog, FormGroup, TextField, DialogContent } from "@material-ui/core"
+import { Typography, Button, Dialog, FormGroup, TextField, DialogContent, DialogActions } from "@material-ui/core"
 import * as OrderCloud from 'ordercloud-javascript-sdk';
 
-const UserShell: OrderCloud.User = {
+const UserShell: OrderCloud.MeUser = {
     Username: '',
     FirstName: '',
     LastName: '',
     Email: '',
+    Password: '',
     Active: true,
 }
 
-export type UserValues = "UserName" | "FirstName" | "LastName" | "Email";
+export type UserValues = "Username" | "FirstName" | "LastName" | "Email" | "Password"; 
 
 const NewUserForm: React.FunctionComponent = () => {
-    const [newUser, setNewUser] = useState<OrderCloud.User>(UserShell);
+    const [newUser, setNewUser] = useState<OrderCloud.MeUser>(UserShell);
     const handleNewUserChange = (key: UserValues) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(key)
         setNewUser({...newUser, [key]: event.target.value})
     }
 
     const SubmitNewUser = () => {
-        return OrderCloud.Me.Register(newUser).then((res) => {
+        var token = OrderCloud.Tokens.GetAccessToken();
+        return OrderCloud.Me.Register(newUser, {
+            anonUserToken: token
+        }).then((res) => {
             console.log(res);
         })
     }
+
     return(
-        <div>
-        <Typography variant="h3">Register</Typography>
-        <TextField
-            id="UserName"
-            label="User Name"
-            variant="outlined"
-            margin="dense"
-            onChange={handleNewUserChange("UserName")}>{newUser.Username}
-        </TextField>
-        <TextField
-            id="UserName"
-            label="User Name"
-            variant="outlined"
-            margin="dense"
-            onChange={handleNewUserChange("UserName")}>{newUser.Username}
-        </TextField>
-        <TextField
-            id="UserName"
-            label="User Name"
-            variant="outlined"
-            margin="dense"
-            onChange={handleNewUserChange("UserName")}>{newUser.Username}
-        </TextField>
-        <TextField
-            id="UserName"
-            label="User Name"
-            variant="outlined"
-            margin="dense"
-            onChange={handleNewUserChange("UserName")}>{newUser.Username}
-        </TextField>
-    </div>
+        <React.Fragment>
+            <DialogContent>
+                <Typography variant="h3">Register</Typography>
+                <TextField
+                    id="UserName"
+                    label="User Name"
+                    variant="outlined"
+                    margin="dense"
+                    onChange={handleNewUserChange("Username")}>{newUser.Username}
+                </TextField>
+                <TextField
+                    id="FirstName"
+                    label="First Name"
+                    variant="outlined"
+                    margin="dense"
+                    onChange={handleNewUserChange("FirstName")}>{newUser.FirstName}
+                </TextField>
+                <TextField
+                    id="LastName"
+                    label="Last Name"
+                    variant="outlined"
+                    margin="dense"
+                    onChange={handleNewUserChange("LastName")}>{newUser.LastName}
+                </TextField>
+                <TextField
+                    id="Email"
+                    label="Email"
+                    variant="outlined"
+                    margin="dense"
+                    onChange={handleNewUserChange("Email")}>{newUser.Email}
+                </TextField>
+                <TextField
+                    id="Password"
+                    label="Password"
+                    variant="outlined"
+                    margin="dense"
+                    onChange={handleNewUserChange("Password")}>{newUser.Password}
+                </TextField>
+            </DialogContent>
+            <DialogActions>
+                <Button type="submit" onClick={SubmitNewUser}>Submit</Button>
+                <Button type="reset">Cancel</Button>
+            </DialogActions>
+        </React.Fragment>
     )
 }
 
@@ -75,7 +94,6 @@ const LoginUser: React.FunctionComponent = () => {
         return OrderCloud.Auth.Login(creds.UserName, creds.passWord, process.env.REACT_APP_CLIENT_ID!, [])
         .then(response => {
             console.log(response);
-            debugger;
             OrderCloud.Tokens.SetAccessToken(response.access_token);
         })
     }
